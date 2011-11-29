d961b15459e877536b79115c08f706d800ef76ab
TK Kocheran <rfkrocktk@gmail.com>
2011-11-29 14:48:35 -0800

    Bug fixeroonie. The fixtures weren't being included: bad news. Should be fixed now.

--- setup.py
+++ setup.py
@@ -2,13 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
 	name = "django-locality",
-	version = "0.0.1-SNAPSHOT",
-	url = 'http://blog.tkassembled.com/locality',
+	version = "0.1.1",
+	url = 'https://github.com/rfkrocktk/django-locality',
 	license = 'AGPL',
 	description = 'Countries and territories, made usable.',
 	author = 'TK Kocheran',
+	author_email = 'rfkrocktk@gmail.com',
 	packages = find_packages('src'),
 	package_dir = {'': 'src'},
+	package_data = {
+		'': ['locality/fixtures/*.json']
+	},
 	install_requires = [
 		'Django>=1.3.0',
 		'setuptools'
