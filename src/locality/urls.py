635ae1495efe4302a5516b1cbcc18977a66d9b22
markflorence728 <markflorence728@gmail.com>
2021-12-09 18:21:33 +0800

    fix urls

--- src/locality/urls.py
+++ src/locality/urls.py
@@ -1,22 +1,22 @@
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls import url
 from django.contrib import admin
+from locality.views import countries, territories, territories_by_country, country, generate_territories
 
 admin.autodiscover()
 
-urlpatterns = patterns(
-    'locality.views',
+urlpatterns = [
     # generate initial data from localflavors for territories
-    # url(r'^json/generate/territories/?$', 'generate_territories'),
+    # url(r'^json/generate/territories/?$', generate_territories, name='generate_territories'),
 
     # get all countries
-    url(r'^(?P<to>(?:json|xml|yaml))/countries/(?:all/?)?$', 'countries'),
+    url(r'^(?P<to>(?:json|xml|yaml))/countries/(?:all/?)?$', countries, name='countries'),
     # get all territories
-    url(r'^(?P<to>(?:json|xml|yaml))/territories/(?:all/?)?$', 'territories'),
+    url(r'^(?P<to>(?:json|xml|yaml))/territories/(?:all/?)?$', territories, name='territories'),
     # get territories by country
     url(r'^(?P<to>(?:json|xml|yaml))/territories(?:/(?P<country>(?:\d+|\w{2}|\w{3}))/?)?$',
-        'territories_by_country'),
+        territories_by_country, name='territories_by_country'),
     # get country
     url(r'^(?P<to>(?:json|xml|yaml))/country/(?P<country>(?:\d+|\w{2}|\w{3}))/?$',
-        'country'),
+        country, name='country'),
     #url(r'^admin/', include(admin.site.urls)),
-)
+]
